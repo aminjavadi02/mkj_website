014ac0593051e8a92e9141b985277197035baec5
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-15 06:55:12 +0330

    items style modifed, background of them is glass and some trash code deleted

--- resources/views/guest/fa/component/item/for_index.blade.php
+++ resources/views/guest/fa/component/item/for_index.blade.php
@@ -20,16 +20,3 @@
 </div>
 
 <!-- more items -->
-
-
-<div class="cardbackground"></div>
-<div class="box col-md-4">
-    <!-- img, name_fa, more info -->
-    @if($item['images'])
-    <div class="item-img" style="background-image:url('{{asset('storage/images/item_images/'.$item['images'][0]['name'])}}')"></div>
-    @else
-    <div class="item-img" style="text-align:center; padding-top:14vh; background:green">no image</div>
-    @endif
-    <div class="name">{{$item['item']}}</div>
-    <a href="#" class="more">بیشتر</a>
-</div>
