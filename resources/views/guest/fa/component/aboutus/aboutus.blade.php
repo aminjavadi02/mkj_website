0abb1e9316da234bbcc8fc6f3b9e613ff52190cd
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-18 12:02:24 +0330

    aboutus page, section2: contact info: done

--- resources/views/guest/fa/component/aboutus/aboutus.blade.php
+++ resources/views/guest/fa/component/aboutus/aboutus.blade.php
@@ -1,22 +1,38 @@
 @if($aboutus['history_fa'])
 <div class="aboutus">
-<div class="aboutus-img-container">
-    <img src="{{asset('assets/img/high-voltage-post-high-voltage-tower.jpg')}}" class="bg">
-</div>
-<div class="container aboutus-big-container">
-    <div class="row">
-        <div class="col-12">
-            <div class="row">
-                <div class="col title">
-                    تاریخچه ی شرکت
+    <div class="aboutus-img-container">
+        <img src="{{asset('assets/img/high-voltage-post-high-voltage-tower.jpg')}}" class="bg">
+    </div>
+    <div class="container aboutus-big-container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col title">
+                        تاریخچه ی شرکت
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col wrapper" dir="rtl" id="history_fa"></div>
+                <div class="row">
+                    <div class="col wrapper" dir="rtl" id="history_fa"></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="contactinfo-container">
+        <div class="contactInfo">
+            <p class="title">اطلاعات تماس</p>
+            <label for="office_phone">تلفن دفتر</label>
+            <p id="office_phone">{{$aboutus['office_phone']}}</p>
+
+            <label for="office_address">آدرس دفتر</label>
+            <p id="office_address">{{$aboutus['office_address_fa']}}</p>
+            
+            <label for="factory_phone">تلفن کارخانه</label>
+            <p id="factory_phone">{{$aboutus['factory_phone']}}</p>
+
+            <label for="factory_address">آدرس کارخانه</label>
+            <p id="factory_address">{{$aboutus['factory_address_fa']}}</p>
+        </div>
+    </div>
 </div>
 <script>
     window.onload = function () {
