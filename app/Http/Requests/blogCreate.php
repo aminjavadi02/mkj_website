3f7b5604eb38bc8553f1e3f40266814c7b883110
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-19 15:34:19 +0330

    slug got removed from blog request

--- app/Http/Requests/blogCreate.php
+++ app/Http/Requests/blogCreate.php
@@ -25,7 +25,6 @@ class blogCreate extends FormRequest
     {
         return [
             'title'=>'required|between:1,255', // between 1 and 255 characters
-            'slug'=>'required|between:1,255',
             'text'=>'required',
         ];
     }
@@ -35,8 +34,6 @@ class blogCreate extends FormRequest
         return [
             'title.required'=>'لطفا عنوان را وارد کنید',
             'title.between'=>'تعداد کاراکترهای عنوان باید حداکثر 255 تا باشد',
-            'slug.required'=>'لطفا اسلاگ را وارد کنید',
-            'slug.between'=>'تعداد کاراکترهای عنوان باید حداکثر 255 تا باشد',
             'text.required'=>'لطفا متن را وارد کنید',
         ];
     }
