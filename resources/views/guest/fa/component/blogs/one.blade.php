baaad6aa0608c9914b8bb227c55517b7aeefb8b4
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-21 14:41:03 +0330

    blog front end done

---
+++ resources/views/guest/fa/component/blogs/one.blade.php
@@ -0,0 +1,15 @@
+@extends('guest.fa.layouts.app')
+@section('content')
+@include('guest.fa.component.blogs.oneheader')
+
+<div class="oneblog-container">
+    <div class="row blog-text" id="blog-text"></div>
+</div>
+
+<script>
+    window.onload = function () {
+        var text = @json($blog['text']);
+        $('#blog-text')[0].innerHTML = text;
+    }
+</script>
+@endsection
