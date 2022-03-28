a6832e47bd11c13265abb3f8c67fff64cd594887
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-28 17:06:04 +0430

    items responsive

--- resources/views/guest/fa/component/card/carditem.blade.php
+++ resources/views/guest/fa/component/card/carditem.blade.php
@@ -5,19 +5,27 @@
     <div class="item-img no-img" style="text-align:center; padding-top:14vh;">no image</div>
     @endif                    
     <div class="card-body">
+        @if($item['name_fa'])
         <h5 class="card-title">{{$item['name_fa']}}</h5>
+        @endif
+        @if($item['size'])
         <div class="info-container">
-            <h5>سایز:</h5>
             <h4>{{$item['size']}}</h4>
+            <h5>سایز</h5>
         </div>
+        @endif
+        @if($item['alloy'])
         <div class="info-container">
-            <h5>آلیاژ:</h5>
             <h4>{{$item['alloy']}}</h4>
+            <h5>آلیاژ</h5>
         </div>
+        @endif
+        @if($item['category'])
         <div class="info-container">
-            <h5>دسته بندی:</h5>
             <h4>{{$item['category']['name_fa']}}</h4>
+            <h5>دسته بندی</h5>
         </div>
+        @endif
         <a href="/one-item/fa/{{$item['id']}}" class="btn btn-dark link"><span>مشاهده</span></a>
     </div>
 </div>
