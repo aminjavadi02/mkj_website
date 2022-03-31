2b5ce86154a5fcdff4c8418a864efcaf5128e73a
aminjavadi02 <aminjavadi02@gmail.com>
2022-03-31 19:51:08 +0430

    makeul function fixed

--- resources/views/guest/fa/component/category/catIndex.blade.php
+++ resources/views/guest/fa/component/category/catIndex.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="backgroundarea">
-    <div class="categoryList-container">
+    <div class="categoryObject-container">
         <ul class="main-list">
         </ul>
     </div>
@@ -13,24 +13,67 @@
 <script src="{{asset('assets/js/core/jquery.min.js')}}"></script>
 <script>
     window.onload = function(){
-        var categoryList = @json($categories);
+        var categoryObject = @json($categories);
+        var categoryArray = objToArray(categoryObject);
         const ul = $('.main-list')
-        makeUl(categoryList,ul[0]);
+        makeUl(categoryArray,ul[0]);
     }
-    
+
+    function objToArray(obj){
+        theArray = [];
+        for(i in obj){
+            theArray.push(obj[i]);
+        }
+        return theArray;
+    }
+
     function makeUl(list,ul) {
-        for( item in list){
-            if(list[item].children.length > 0){
-                const UlTag = document.createElement('ul');
-                UlTag.innerText = list[item].name_fa;
-                UlTag.classList.add('sub-list');
-                ul.appendChild(UlTag);
-                makeUl(list[item].children,UlTag);
+        for(i in list){
+            if(list[i].parent_id == null){
+                // is root
+                if(list[i].children.length > 0){
+                    // is root and ul
+                    const UlTag = document.createElement('ul');
+                    UlTag.innerText = list[i].name_fa;
+                    UlTag.parent_id = list[i].parent_id;
+                    UlTag.id = list[i].id;
+                    UlTag.classList.add('root-list');
+                    ul.appendChild(UlTag);
+                    // children is array of objects. no need to make array
+                    makeUl(list[i].children,UlTag);
+                }
+                else{
+                    // is root and li
+                    const LiTag = document.createElement('li');
+                    LiTag.innerText = list[i].name_fa;
+                    LiTag.parent_id = list[i].parent_id;
+                    LiTag.id = list[i].id;
+                    LiTag.classList.add('root-list');
+                    ul.appendChild(LiTag);
+                }
             }
             else{
-                const LiTag = document.createElement('li');
-                LiTag.innerText = list[item].name_fa;
-                ul.appendChild(LiTag);
+                // is not root. parent_id == something else's id
+                if(list[i].children.length > 0){
+                    // is ul
+                    const UlTag = document.createElement('ul');
+                    UlTag.innerText = list[i].name_fa;
+                    UlTag.parent_id = list[i].parent_id;
+                    UlTag.id = list[i].id;
+                    UlTag.classList.add('sub-list');
+                    ul.appendChild(UlTag);
+                    // children is array of objects. no need to make array
+                    makeUl(list[i].children,UlTag);
+                }
+                else{
+                    // is li
+                    const LiTag = document.createElement('li');
+                    LiTag.innerText = list[i].name_fa;
+                    LiTag.parent_id = list[i].parent_id;
+                    LiTag.id = list[i].id;
+                    LiTag.classList.add('sub-list');
+                    ul.appendChild(LiTag);
+                }
             }
         }
     }
